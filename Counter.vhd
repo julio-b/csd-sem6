18745b155b66aef3d2ac6cd82934b9cef53094fb
Julio B <julio.bacel@gmail.com>
2018-06-10 18:02:31 +0300

    fix(?) generic

--- Counter.vhd
+++ Counter.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-package array_for_N_A_dir_signals is        
+package array_for_N_A_dir_signals is
   Subtype dir_signals        is signed(1 downto 0);
   Type    array_for_N_A_dir_signals        is array (natural range <>) of dir_signals;
 end package;
@@ -17,75 +17,43 @@ use array_for_N_A_dir_signals.all;
 entity Counter is 
 generic ( N : natural:= 2 );
 Port ( 
-		CLK : in  std_logic;
-      WE : in  std_logic;
-      DIN : in  unsigned(2*N -1 downto 0);
-      FULL : out  std_logic;
-      EMPTY : out  std_logic;
-		PARKFREE : out unsigned(3 downto 0)
-		);
+	CLK : in  std_logic;
+	WE : in  std_logic;
+	DIN : in  unsigned(2*N -1 downto 0);
+	FULL : out  std_logic;
+	EMPTY : out  std_logic;
+	PARKFREE : out unsigned(3 downto 0)
+	);
 end Counter;
 
-architecture Behavioral of Counter is 
-component Gate_System is 
-Port (
-		clk : in  std_logic;
-		reset : in std_logic;
-		sensor : in  unsigned(1 downto 0);
-		direction : out  signed(1 downto 0)
-		);
-end component ; 
+architecture Behavioral of Counter is
 
-type array_for_N_sensor_signals is array (0 to N-1 ) of unsigned (1 downto 0);
-
-signal N_i : array_for_N_sensor_signals;
 signal N_middle_dir : array_for_N_A_dir_signals (0 to N-1 );
 
 begin
-N_Gates :
-for i in 0 to N-1 generate 
+	N_Gates :
+	for i in 0 to N-1 generate
+	begin
+		Gate_N_i:
+		entity work.Gate_System
+		port map (
+			CLK => CLK ,
+			reset => WE ,
+			sensor => DIN(2*i+1 downto 2*i),
+			direction => N_middle_dir(i)
+		) ;
+	end generate N_Gates;
 
-	begin 
-	
-	N_i(0) <= DIN((2*N-1) downto (2*N-2));
-	N_i(i) <= DIN(N-1 downto 0);
-		Gate_N_0: 
-		if (i =0 ) generate
-						begin
-							Gate_N_0: entity work.Gate_System
-							port map (
-										CLK => CLK ,
-										reset => WE ,
-										sensor => N_i(0),
-										direction => N_middle_dir(0)
-										) ;
-		
-		end generate Gate_N_0 ;
+	Counterx:
+	entity work.Counterx(Behavioral)
+	port map (
+		CLK => CLK,
+		WE => WE ,
+		DIN => DIN(3 downto 0),
+		FULL => FULL,
+		EMPTY => EMPTY,
+		PARKFREE => PARKFREE,
+		N_A_dir => N_middle_dir
+	);
 
-		Gate_N_i: if (i > 0 ) generate
-						begin
-							Gate_N_0: entity work.Gate_System
-							port map (
-										CLK => CLK ,
-										reset => WE ,
-										sensor => N_i(i),
-										direction => N_middle_dir(i)
-										) ;
-		
-		end generate Gate_N_i ;
-		
-		Counterx:
-							entity work.Counterx(Behavioral)
-							port map (
-										CLK => CLK,
-										WE => WE ,
-										DIN => DIN,
-										FULL => FULL,
-										EMPTY => EMPTY,
-										PARKFREE => PARKFREE,			
-										N_A_dir => N_middle_dir(i)
-										);
-
-				 
-end generate N_Gates;
 end Behavioral;
--- Counterx.vhd
+++ Counterx.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-package array_for_N_A_dir_signals is        
+package array_for_N_A_dir_signals is
   Subtype dir_signals        is signed(1 downto 0);
   Type    array_for_N_A_dir_signals        is array (natural range <>) of dir_signals;
 end package;
@@ -16,22 +16,19 @@ entity Counterx is
 generic ( N : natural:= 2 );
 
 Port ( 
-		CLK : in  std_logic;
-      WE : in  std_logic;		
-		N_A_dir: in signed (2*N-1 downto 0 );--array_for_N_A_dir_signals( 0 to N-1) ; 
-      DIN : in  unsigned(2*N-1 downto 0);
-      FULL : out  std_logic;
-      EMPTY : out  std_logic;
-		PARKFREE : out unsigned(3 downto 0)
-		);
+	CLK : in  std_logic;
+	WE : in  std_logic;
+	N_A_dir: array_for_N_A_dir_signals( 0 to N-1);
+	DIN : in  unsigned(3 downto 0);
+	FULL : out  std_logic;
+	EMPTY : out  std_logic;
+	PARKFREE : out unsigned(3 downto 0)
+	);
 end Counterx;
 
 architecture Behavioral of Counterx is
--------------------Declaration of Type------------------------|
-type array_for_N_AnB_signals is array (0 to N-1 ) of signed (3 downto 0); 
 -------------------Declaration of Signals------------------------|
-signal N_A_middle_dir : array_for_N_A_dir_signals( 0 to N-1) ;
-signal N_AnB : array_for_N_AnB_signals ;
+signal N_AnB : unsigned(3 downto 0);
 signal PARKTOTAL: unsigned(3 downto 0); --MA? 15
 signal PARK_CNT: unsigned(3 downto 0); --MA? 15
 -------------------Procedure------------------------|
@@ -54,58 +51,26 @@ begin
 end;
 								
 begin
--------------------For_Loop------------------------|
-N_Input : for i in 0 to N-1 generate
- --  N_A_middle_dir(i) <= N_A_dir(i) ;
- 
- Gate_N_0: 
-		if (i =0 ) generate
-						begin
-						N_AnB(0) <= resize(N_A_dir(0), N_AnB(0)'length + "0000");
-						PARKFREE <= PARKTOTAL - PARK_CNT;
-						EMPTY <= '1' when PARK_CNT = 0 else '0' ;
-						FULL <= '1' when PARK_CNT = PARKTOTAL else '0';
-		end generate Gate_N_0 ;
-		
-		Gate_N_1: 
-		if (i =1 ) generate
-						begin
-						N_AnB(i) <= resize(N_A_dir(0), N_AnB(0)'length) + resize(N_A_dir(i), N_AnB(i)'length);
-						PARKFREE <= PARKTOTAL - PARK_CNT;
-						EMPTY <= '1' when PARK_CNT = 0 else '0' ;
-						FULL <= '1' when PARK_CNT = PARKTOTAL else '0';
-		end generate Gate_N_1 ;
-		
-		Gate_N_1: 
-		if (i >1 ) generate
-						begin
-						N_AnB(i) <= resize(N_A_dir(i-1), N_AnB(i-1)'length) + resize(N_A_dir(i), N_AnB(i)'length);
-						PARKFREE <= PARKTOTAL - PARK_CNT;
-						EMPTY <= '1' when PARK_CNT = 0 else '0' ;
-						FULL <= '1' when PARK_CNT = PARKTOTAL else '0';
-		end generate Gate_N_0 ;
-		
---	N_AnB(i) <= resize(N_A_dir(0), N_AnB(0)'length) + resize(N_A_dir(i), N_AnB(i)'length);
---	PARKFREE <= PARKTOTAL - PARK_CNT;
---	EMPTY <= '1' when PARK_CNT = 0 else '0' ;
---	FULL <= '1' when PARK_CNT = PARKTOTAL else '0';
+	sum_N: process(N_A_dir)
+		variable tSUM: signed(3 downto 0);
+	begin
+		tSUM := (others => '0');
+		for i in N_A_dir'length loop
+			tSUM := tSUM + resize(N_A_dir(i), tSUM'length);
+		end loop;
+		N_AnB <= tSUM;
+	end process sum_N;
 
    --------------Count Process---------------|
-	count : process(CLK, N_AnB(i), WE ) is
+	count : process(CLK, N_AnB, WE ) is
 	begin
 		if WE = '1' then
 			PARK_CNT <= (others => '0');
 		elsif rising_edge(CLK) then
-			PARK_CNT <= unsigned(signed(std_logic_vector(PARK_CNT)) + N_AnB(i));
+			PARK_CNT <= unsigned(signed(std_logic_vector(PARK_CNT)) + N_AnB);
 		end if;
-		
 	end process count;
-	
-end generate N_Input ;
-----------------End Of For Loop---------------------|
 
-   Check_signals (  DIN , PARKTOTAL, PARK_CNT); -------procedure---------|
-	
    ----------------Register-----------------|
 	The_smallest_process_ever :process (CLK) is	
 	begin 
@@ -116,7 +81,9 @@ end generate N_Input ;
 		end if;		
 	end process	The_smallest_process_ever ;
 
+	Check_signals (  DIN , PARKTOTAL, PARK_CNT); -------procedure---------|
+	PARKFREE <= PARKTOTAL - PARK_CNT;
+	EMPTY <= '1' when PARK_CNT = 0 else '0' ;
+	FULL <= '1' when PARK_CNT = PARKTOTAL else '0';
 
 end Behavioral;
-								
-								
